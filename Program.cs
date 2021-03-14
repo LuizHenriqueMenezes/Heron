f94f06aba76b7d83aac5039b6e4eb423bc897b34
Luiz Henrique <lmenezesmessias@gmail.com>
2021-03-14 15:15:14 -0300

    Finalização

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Heron
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite os lados do triângulo desejado: ");
+
+            Console.WriteLine("Lado 1: ");
+            double pri = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Lado 2: ");
+            double seg = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Lado 3: ");
+            double ter = Convert.ToDouble(Console.ReadLine());
+
+           double semiperimetro = (pri + seg + ter) / 2;
+           Console.WriteLine($"Semiperímetro: {semiperimetro}");
+
+           // Dividi em várias linhas pois quando pus em uma linha só o resultado não saia
+           double areaUM = semiperimetro - pri; 
+           double areaDois = semiperimetro - seg;
+           double areaTres = semiperimetro - ter;  
+           double area = semiperimetro * areaUM * areaDois * areaTres; 
+           double areaResultado = Math.Sqrt(area); 
+           Console.WriteLine($"Área: {areaResultado}");
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Heron
+
 Calcule a área de um triângulo qualquer, dadas as medidas dos 3 lados. Exiba o semiperímetro e a área.
+
+```cs
+Digite os lados do triângulo desejado.
+
+Lado 1..: 3
+Lado 2..: 25
+Lado 3..: 26
+
+Semiperímetro..: 27
+Área...........: 36
+```
